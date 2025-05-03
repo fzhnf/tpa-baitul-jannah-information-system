7c2a4f20088aecd4c104d0bcc8022c0e441d06c9
tima gutmensch <tima_gutmensch+github@proton.me>
2025-05-03 20:05:08 +0800

    chore: change semester class label to just say class instead semester class

--- app/Filament/Resources/SemesterResource/RelationManagers/SemesterClassesRelationManager.php
+++ app/Filament/Resources/SemesterResource/RelationManagers/SemesterClassesRelationManager.php
@@ -7,29 +7,33 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SemesterClassesRelationManager extends RelationManager
 {
     protected static string $relationship = 'semesterClasses';
 
+    protected static ?string $title = 'Classes';
+
+    protected static ?string $label = 'Class';
+
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('id')
+                Forms\Components\TextInput::make('nama_semester_class')
                     ->required()
-                    ->maxLength(255),
+                    ->maxLength(255)
+                    ->label('Name'),
             ]);
     }
 
     public function table(Table $table): Table
     {
         return $table
-            ->recordTitleAttribute('id')
+            ->recordTitleAttribute('nama_semester_class')
             ->columns([
-                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('nama_semester_class')
+                    ->label('Name'),
             ])
             ->filters([
                 //
