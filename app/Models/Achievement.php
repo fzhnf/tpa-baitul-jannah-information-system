066f3f8323152179d72a4f3b6799b2231d63fd5d
tima gutmensch <tima_gutmensch+github@proton.me>
2025-05-10 17:58:23 +0800

    feat(filament): add column name to achievement

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * 
- *
  * @property int $id
  * @property string $achievement_name
  * @property string $category
@@ -16,6 +14,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\StudentAchievement> $studentAchievements
  * @property-read int|null $student_achievements_count
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Achievement newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Achievement newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Achievement query()
@@ -24,6 +23,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Achievement whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Achievement whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Achievement whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class Achievement extends Model
@@ -35,6 +35,9 @@ class Achievement extends Model
         'category',
     ];
 
+    /**
+     * @return HasMany<StudentAchievement,Achievement>
+     */
     public function studentAchievements(): HasMany
     {
         return $this->hasMany(StudentAchievement::class);
